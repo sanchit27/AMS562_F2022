d95b2a26b7d84af8e2564fbb1ddb4805d55af3eb
Sanchit Bhargava <>
2022-09-28 15:11:02 -0400

    Enter and print date of Birth Details

--- homeworks/hw2/hw2.cpp
+++ homeworks/hw2/hw2.cpp
@@ -2,9 +2,15 @@
 using namespace std;
 int main()
 {
+    //Name variables
     string title;
     string firstname;
     string lastname;
+
+    //DOB variables
+    string month;
+    string date;
+    string year;
 
     cout<<"Enter title for the name \n";
     cin>>title;
@@ -13,7 +19,17 @@ int main()
      cout<<"Enter your last name \n";
     cin>>lastname;
     //cout<<"\n";
+
+
+    cout<<"Enter your DOB month \n";
+    cin>>month;
+    cout<<"Enter your DOB date \n";
+    cin>>date;
+    cout<<"Enter your DOB year \n";
+    cin>>year;
+
     cout<<"\n"<<title<<" "<<firstname<<" "<<lastname<<"\n";
+    cout<<"\t DOB:"<<month<<"/"<<date<<"/"<<year<<endl;
 
     return 0;
 }
