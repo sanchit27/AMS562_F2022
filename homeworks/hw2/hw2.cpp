b67f412fb48d4cd4207dec76cd6205fc762ac673
Sanchit Bhargava <>
2022-09-27 20:54:10 -0400

    Homework 2 first name, last name and title commit

---
+++ homeworks/hw2/hw2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string title;
+    string firstname;
+    string lastname;
+
+    cout<<"Enter title for the name \n";
+    cin>>title;
+    cout<<"Enter your first name \n";
+    cin>>firstname;
+     cout<<"Enter your last name \n";
+    cin>>lastname;
+    //cout<<"\n";
+    cout<<"\n"<<title<<" "<<firstname<<" "<<lastname<<"\n";
+
+    return 0;
+}
