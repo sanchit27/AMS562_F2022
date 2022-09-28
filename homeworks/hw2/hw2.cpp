0311f619b5394e6e30c80ea10d164f8962fe8d35
Sanchit Bhargava <>
2022-09-28 16:11:32 -0400

    Enter Bio Details and comments

--- homeworks/hw2/hw2.cpp
+++ homeworks/hw2/hw2.cpp
@@ -12,15 +12,20 @@ int main()
     string date;
     string year;
 
+    //Bio variables
+    string bio;
+   
+
+    //Taking user input for name data
     cout<<"Enter title for the name \n";
     cin>>title;
     cout<<"Enter your first name \n";
     cin>>firstname;
-     cout<<"Enter your last name \n";
+    cout<<"Enter your last name \n";
     cin>>lastname;
-    //cout<<"\n";
+   
 
-
+    //Taking user input for DOB data
     cout<<"Enter your DOB month \n";
     cin>>month;
     cout<<"Enter your DOB date \n";
@@ -28,8 +33,20 @@ int main()
     cout<<"Enter your DOB year \n";
     cin>>year;
 
-    cout<<"\n"<<title<<" "<<firstname<<" "<<lastname<<"\n";
+
+  
+    cout << "Please enter your Bio: \n";
+    cin.ignore();
+    getline(cin, bio);
+    
+  
+    //Display name details
+    cout<<"\n"<<title<<" "<<firstname<<" "<<lastname<<endl;
+    //Display DOB details
     cout<<"\t DOB:"<<month<<"/"<<date<<"/"<<year<<endl;
+
+    //Display Bio details
+    cout<<"\t BIO:"<<bio<<endl;
 
     return 0;
 }
